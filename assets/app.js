5b180c6fac56b017558f391e0372c8e3296910a4
rohanitabar <a.rohanitabar@gmail.com>
2024-11-14 14:53:36 +0330

    git init

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+$('.carousel').carousel({  
+  interval: 3000, 
+  pause: 'hover'  
+});
+$(document).ready(function () {  
+  $('#carouselExampleFade').carousel({  
+      interval: 4000
+  });  
+}); 
